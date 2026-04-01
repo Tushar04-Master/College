1951edabd1d8070cd48fc950d490617fc71c2b26
Tushar04-Master <tus88122@gmail.com>
2026-04-01 17:33:57 +0530

    1st April

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count  
 
 def post_list(request, tag_slug=None):
     post_list=Post.published.all()
@@ -14,7 +15,7 @@ def post_list(request, tag_slug=None):
         tag=get_object_or_404(Tag,slug=tag_slug)
         post_list=post_list.filter(tags__in=[tag])  
 
-    paginator = Paginator(post_list,10)
+    paginator = Paginator(post_list,3) # 3 posts in each page
     page_number = request.GET.get('page',1)
     try:
         posts = paginator.page(page_number)
@@ -35,12 +36,15 @@ def post_detail(request,year,month,day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(matching_tags=Count('tags')).order_by('-matching_tags','-publish')
     
     # GET ALL ACTIVE COMMENTS
     active_comments=post.comments.filter(active=True)
     #Initialize the comment form
     comment_form=CommentForm()
-    return render(request,'blog/post/detail.html',{'post':post , 'comments':active_comments , 'form':comment_form})
+    return render(request,'blog/post/detail.html',{'post':post , 'comments':active_comments , 'form':comment_form, 'similar_posts':similar_posts})
 
 def post_share(request , post_id):
     post=get_object_or_404(Post,id=post_id , status=Post.Status.PUBLISHED)
